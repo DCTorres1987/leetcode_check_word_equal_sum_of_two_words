3fefe81451234a3c299eaa2048709cddf7dbc8e0
DCTorres87 <cruz_danielle_torres@yahoo.com>
2021-06-09 22:15:11 -0400

    work on solving algorithm

--- codesolution.js
+++ codesolution.js
@@ -1,18 +1,19 @@
-const isSumEqual = (firstWord, secondWord, targetWord) => {
+var isSumEqual = function(firstWord, secondWord, targetWord) {
     let letters = new Set(['a','b','c','d','e','f','g','h','i','j']);
+    let num1 = [];
+    let num2 = [];
+    let targetWordIndex = [];
     
-    let array = [firstWord.split(""),secondWord.split(""),targetWord.split("")];
-    let newArray = [];
-    
-    for (let i = 0; i < array.length; i++){
-        
-       for (let k=0; k < array[i].length; k++){
-           newArray.push([...letters].indexOf(array[i][k]))
-           }
-       }
+    for(let a = 0; a < firstWord.length; a++){
+        num1.push([...letters].indexOf(firstWord[a]))
     }
-    console.log(array);
     
-}
+    for(let b = 0; b < secondWord.length; b++){
+    num2.push([...letters].indexOf(secondWord[b]))
+    }
+    
+  console.log(parseInt(num1.join("")) + parseInt(num2.join("")))
+    
+};
 
 console.log(isSumEqual('acb','cba','cdb'));
